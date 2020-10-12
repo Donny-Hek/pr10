5ffa4054ce2ffdb114e9e495742651d31c133ce9
Ira M <klewernoe@gmail.com>
2020-10-12 23:45:29 +0300

    Полная практическая №10

--- src/Task3.java
+++ src/Task3.java
@@ -2,19 +2,18 @@ import java.util.Scanner;
 
 public class Task3 {
     public static void arif(int N){
-        int n=10;
-        if (N/10>0) {
-            n=chis(N,n)/10;
+        if (N/10>0){
+            int n=chis(N,10)/10;
             System.out.println(N/n);
             N=N-(N/n)*n;
             arif(N);
-        } else System.out.println(N);
+        } else System.out.println(N);//однозначное или последняя цифра
     }
 
-    static int chis(int N, int n) {
+    static int chis(int N, int n) {//ПРАВИЛЬНО
         if (N/n>0) {
             n*=10;
-            chis(N,n);
+            n=chis(N,n);
         }
         return n;
     }
